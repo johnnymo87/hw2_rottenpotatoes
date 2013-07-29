7124dcc0cbc4a02d1d806e8bd92778c7bf21ab62
johnnymo87 <johnnymo87@gmail.com>
2013-07-29 15:02:25 -0700

    1B finished, less CSS 'hiliting'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,7 @@
 module ApplicationHelper
   def sortable(column, title=nil)
     title ||= column.titleize
-    css_class = column == sort_column ? 'hilite' : nil
     direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
-    link_to title, movies_path(:sort => column, :direction => direction), {:class => css_class, :id => "#{column}_header"}
+    link_to title, movies_path(:sort => column, :direction => direction), :id => "#{column}_header"
   end
 end
